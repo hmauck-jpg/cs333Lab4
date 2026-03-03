d361e6b5a32035a6ae5281101f11a0369c95d663
Haleah Mauck <hmauck@pdx.edu>
2026-03-03 12:21:29 -0800

    automatic commit

--- monitorasaurus.c
+++ monitorasaurus.c
@@ -13,28 +13,59 @@
 #include <unistd.h>
 #include <getopt.h>
 #include <string.h>
+#include <signal.h>
+#include <semaphore.h>
+#include <errno.h>
+#include <time.h>
+#include "monitorasaurus.h"
+
+
+ 
+#ifdef NOISY_DEBUG 
+# define NOISY_DEBUG_PRINT fprintf(stderr, "%s %s %d\n", __FILE__, __func__, __LINE__)
+#else // NOISY_DEBUG
+# define NOISY_DEBUG_PRINT
+#endif // NOISY_DEBUG
 
 static int v = 0;
+volatile sig_atomic_t run =  1;
+
+
+// Desc: Signal handler function, sets the global run variable to false
+// Inputs: int, the signal 
+// Returns: void 
+void handleSignal(int signal);
+
+// Desc: Runs a loop of refreshing the screen, at the user specifed interval
+// Inputs: ints, the amount of times the screen has refreshed, and the interval at which to refresh
+// a unsigned int, the width to display on screen, a semphore to use as a counter
+// Returns: Int, 1 if successful, 0 if popen fails 
+int refresh(int refreshCount, int refreshInterval, size_t width, sem_t * bobSemaphore);
+ 
 
 int main(int argc, char * argv[]) {
 
-    int refreshInterval = 2;
+    int refreshInterval = DEFAULT_INTERVAL;
     int refreshCount = 0;
-    int width = 100;
+    size_t width = DEFAULT_COLS;
+    struct sigaction bobSignal;
+    //initalize semphore, value init to 0, use sem_init(), second param also 0
+    sem_t bobSemaphore;
+    sem_init(&bobSemaphore, 0, 0);
 
     {
 
-        int opt -1;
+        int opt = -1;
         while ((opt = getopt(argc, argv, OPTIONS)) != -1) {
             switch (opt) {
                 case 'i':
-                    refreshInterval = optarg;
+                    refreshInterval = atoi(optarg);
                      break;
                 case 'c':
-                    refreshCount = optarg;
+                    refreshCount = atoi(optarg);
                     break;
                 case 'w':
-                    width = optarg;
+                    width = atoi(optarg);
                     break;
                 case 'v':
                     v = 1;
@@ -53,20 +84,93 @@ int main(int argc, char * argv[]) {
                     exit(EXIT_FAILURE);
                     break;
             }
+        }
     }
-}
 
+    //connect signal handler to signalHandle function 
+    memset(&bobSignal, 0, sizeof(bobSignal));
+    bobSignal.sa_handler = handleSignal;
+    if (sigaction(SIGINT, &bobSignal, NULL)== -1) {
+        fprintf(stderr, "Sigaction failed\n");
+        exit(EXIT_FAILURE);
+    }
 
+    //call refresh loop function
+    if (refresh(refreshCount, refreshInterval, width, &bobSemaphore) == 0) {
+        fprintf(stderr, "Semaphore never reached timeout\n");
+        exit(EXIT_FAILURE);
+    }
 
-
-
-
-
-
-
-
+    //destroy semaphore
+    sem_destroy(&bobSemaphore); 
+ 
     exit(EXIT_SUCCESS);
 
-
 }
+
+
+// Desc: Signal handler function, sets the global run variable to false
+// Inputs: int, the signal 
+// Returns: void 
+void handleSignal(int signal) {
+    run = 0;
+}
+
+
+// Desc: Runs a loop of refreshing the screen, at the user specifed interval
+// Inputs: ints, the amount of times the screen has refreshed, and the interval at which to refresh
+// a unsigned int, the width to display on screen, a semphore to use as a counter
+// Returns: Int, 1 if successful, 0 if popen fails 
+int refresh(int refreshCount, int refreshInterval, size_t width, sem_t * bobSemaphore) {
+    int count = 0;
+    struct timespec ts;
+    FILE * bob = NULL;
+    char line[MAX_LINE_LEN];
+    int success = 1;
+        
+    //while (run (run is set to false when user presses control c) && count < refreshCount)
+    while(run && (refreshCount == INFINITE_COUNT || count < refreshCount)) {
+
+        clock_gettime(CLOCK_REALTIME, &ts);
+        ts.tv_sec += refreshInterval;
+        //pass timespec as second param to sem_timedwait()
+        //what is the first parameter?
+        if (sem_timedwait(bobSemaphore, &ts) == -1) {
+
+            //wait for timeout, the return value with be -1
+            //the value for magic gloably variable errno is set
+            if(errno == EINTR) {
+                //control c was pressed, break out of while loop 
+                break;
+            }
+            else if (errno == ETIMEDOUT) {
+                //semphore timeout occured
+                //update display 
+                //clear the screen,  
+                printf(CLEAR_SCREEN);
+
+                //process command output in popen loop
+                bob = popen(CMD, "r");
+                if (bob == NULL) {
+                    fprintf(stderr, "popen failed\n");
+                    success = 0;
+                    break;
+                }
+                while (fgets(line, sizeof(line), bob)) {
+                    if (strlen(line) > width) {
+                        line[width] = '\n';
+                        line[width + 1] = '\0';
+                    }
+                    fputs(line, stdout);
+                }
+                pclose(bob);
+                ++count;
+            }
+     
+        }
+    }
+    
+    return success;
+}
+
   
--- monitorasaurus.h
+++ monitorasaurus.h
@@ -1,20 +1,51 @@
-// Haleah Mauck
-// 2/24/2026
-// CS-314-006
-// Lab4 monitorasaurus
-// hmauck@pdx.edu
-// this is the header file for the monitorasaurus program
+// R Jesse Chaney
+// rchaney@pdx.edu
 
+// Include guards!!!
+#ifndef _MONITORASAURUS_H
+# define _MONITORASAURUS_H
 
+// This is the, arbitary, value i use for what i read with fgets().
+# define MAX_LINE_LEN 5000
 
-#define OPTIONS "icwvh"
-#define NICE_INCREMENT 10
+// This is the command sent to popen(). there are a couple of other
+// examples on here you can play with. Just be sure to leave the last
+// one defined when you submit your assignment.
+# ifndef CMD
+//#  define CMD "free -m"
+//#  define CMD "uptime"
+//#  define CMD "ps aux | grep ${LOGNAME} | sort -nrk 3,3 | head -n 5"
+#  define CMD "ps augx --sort=-pcpu | head -n 20"
+# endif // CMD
 
+# define OPTIONS "i:c:w:vh"
 
-#ifdef NOISY_DEBUG 
-# define NOISY_DEBUG_PRINT fprintf(stderr, "%s %s %d\n", __FILE__, __func__, __LINE__)
-#else // NOISY_DEBUG
-# define NOISY_DEBUG_PRINT
-#endif // NOISY_DEBUG
+// This is the default value use between updates to the display.
+// 2 seconds.
+# define DEFAULT_INTERVAL 2
 
-static int v = 0;
+// I don't allow the interval to be less than 1 second. technically, we could
+// go into some number of nanoseconds. i'm not going there.
+# define MIN_INTERVAL 1
+
+// The maximum interval between updates.
+# define MAX_INTERVAL 10
+
+// If the user enters something really wacked out for the count of the
+// number of times to update the display (like something that is not a
+// number or a negative value), i just fallback to this.
+# define FALLBACK_COUNT 5
+
+// This means the user must press Control-c to exit.
+# define INFINITE_COUNT 0
+
+// The maximum width of a string to print to the screen. If the string
+// returned from reading output from popen() exceeds this value,
+// truncate it.
+# define DEFAULT_COLS 100
+
+// This is the value you pass to printf() to clear the screen each time
+// before an display update.
+# define CLEAR_SCREEN "\033[2J\033[H"
+
+#endif // _MONITORASAURUS_H
